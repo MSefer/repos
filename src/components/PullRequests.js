69e81f5f96abcefbbbf1b78cfa482f5184223170
muserref <muserrefsefer@gmail.com>
2019-12-11 22:49:26 +0300

    Add pull requests.

--- src/components/PullRequests.js
+++ src/components/PullRequests.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import '../App.css';
+import { useState, useEffect } from 'react';
+import { useStateValue } from '../context/repoContext'
+import axios from 'axios';
 
-function PullRequests({ match }) {
+function PullRequests() {
+  const [pullRequests, setPullRequests] = useState({ pullRequests: [] });
+  const [state, dispatch] = useStateValue();
+  const [isLoading, setIsLoading] = useState(true);
+  const { repo } = state;
+
+  useEffect(() => {
+    async function getIssues() {
+      let response = await axios(`https://api.github.com/repos/reactjs/${repo.name}/pulls`);
+      setPullRequests(response.data);
+      setIsLoading(false);
+    }  
+    getIssues();
+  }, []);
+
   return (
     <React.Fragment>
-      pull requests
+      <ul className="wrapper">
+        {isLoading ? 'Yükleniyor...' : pullRequests.map(pullRequest => (
+          <li className="list-item" key={pullRequest.id}>
+            {pullRequest.title}
+          </li>
+        ))}
+      </ul>
     </React.Fragment>
   );
 }
